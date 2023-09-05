eda2954a905f196bf4bcce20f3d4a41fe1e56d75
Dmytro-Lys <121360853+Dmytro-Lys@users.noreply.github.com>
2023-09-05 14:36:27 +0000

    Deploying to gh-pages from @ Dmytro-Lys/goit-react-hw-08-phonebook@5299fe8290f0558ffd43b49592ac0e54a8c14aea 🚀

---
+++ static/js/304.c8059d7d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[304],{6304:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var a="Home_container__AHomM",r="Home_title__qAdt+",o=n(184);function c(){return(0,o.jsx)("div",{className:a,children:(0,o.jsx)("h1",{className:r,children:"Phonebook manager"})})}}}]);
+//# sourceMappingURL=304.c8059d7d.chunk.js.map
